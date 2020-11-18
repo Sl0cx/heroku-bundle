ae6a3039da6e668684b066e7c236b178d3815895
Sl0cx <74133461+Sl0cx@users.noreply.github.com>
2020-11-18 20:00:38 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
+
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
